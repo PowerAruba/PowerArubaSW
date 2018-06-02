1524f182f3e28e7171afbbc6d0a2c3d22db21d7c
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-06-02 16:44:45 +0200

    System: Add Set-ArubaSWSystem cmdlet
    can update Name, Location and Contact of switch

--- PowerArubaSW/Public/System.ps1
+++ PowerArubaSW/Public/System.ps1
@@ -37,6 +37,61 @@ function Get-ArubaSWSystem {
     }
 }
 
+function Set-ArubaSWSystem {
+
+    <#
+        .SYNOPSIS
+        Set system info about ArubaOS Switch (Provision)
+
+        .DESCRIPTION
+        Set System Info (Name, location, contact)
+
+        .EXAMPLE
+        Set-ArubaSWSystem -name PowerArubaSW -Location PowerArubaSW-Lab -Contact power@arubasw
+
+        Set Aruba Switch system information (like name, location or contact)
+
+    #>
+
+    Param(
+        [Parameter(Mandatory = $false)]
+        [String]$name,
+        [Parameter(Mandatory = $false)]
+        [String]$location,
+        [Parameter(Mandatory = $false)]
+        [String]$contact
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $url = "rest/v4/system"
+
+        $system = new-Object -TypeName PSObject
+
+        if ( $PsBoundParameters.ContainsKey('name') ) {
+            $system | add-member -name "name" -membertype NoteProperty -Value $name
+        }
+
+        if ( $PsBoundParameters.ContainsKey('location') ) {
+            $system | add-member -name "location" -membertype NoteProperty -Value $location
+        }
+
+        if ( $PsBoundParameters.ContainsKey('contact') ) {
+            $system | add-member -name "contact" -membertype NoteProperty -Value $contact
+        }
+
+        $response = invoke-ArubaSWWebRequest -method "PUT" -url $url -Body $system
+
+        $response.content | convertfrom-json
+    }
+
+    End {
+    }
+}
+
 function Get-ArubaSWSystemStatus {
 
     <#
