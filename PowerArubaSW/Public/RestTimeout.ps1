c2512304d57c331146291345ec6486a54a4491b7
Cédric Moreau <moreaucedric0@gmail.com>
2018-07-05 09:11:51 +0200

    Fix range check

--- PowerArubaSW/Public/RestTimeout.ps1
+++ PowerArubaSW/Public/RestTimeout.ps1
@@ -1,11 +1,9 @@
-﻿
-#
+﻿#
 # Copyright 2018, Alexis La Goutte <alexis.lagoutte at gmail dot com>
 # Copyright 2018, Cédric Moreau <moreaucedric0 at gmail dot com>
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 
 function Get-ArubaSWRestSessionTimeout {
 
@@ -64,8 +62,9 @@ function Set-ArubaSWRestSessionTimeout {
     #>
 
     Param(
-        [Parameter (Mandatory=$true, Position=1)]
+        [Parameter (Mandatory=$true, Position=1, ValidateRange(120,7200))]
         [int]$timeout
+        
     )
 
     Begin {
@@ -81,18 +80,13 @@ function Set-ArubaSWRestSessionTimeout {
         {
             $time | add-member -name "timeout" -membertype NoteProperty -Value $timeout
         }
-        if (($timeout -lt 7200) -and ($timeout -gt 120))
-        {
+        
         $response = invoke-ArubaSWWebRequest -method "PUT" -body $time -url $url
 
         $run = ($response | convertfrom-json).timeout
 
         $run
-        }
-        else 
-        {
-        Write-Host "The value you entered ($timeout) is not between 120 and 7200. Please enter a value which is between 120 and 7200"
-        }
+        
 
     }
 
