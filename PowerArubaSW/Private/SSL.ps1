b9e0096afd9410c8bd8af15869fa6e9e598f5a44
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-08-04 12:22:20 +0200

    SSL(TLS): add SSL.ps1 from PowerArubaCP module
    With Set-ArubaSWCipherSSl for allowing TLS 1.1 and TLS 1.2
    and Set-ArubaSWuntrustedSSL cmdet for disable SSL chain Validation

---
+++ PowerArubaSW/Private/SSL.ps1
@@ -0,0 +1,31 @@
+#
+# Copyright 2018, Alexis La Goutte <alexis.lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+Function Set-ArubaSWuntrustedSSL {
+
+  # Hack for allowing untrusted SSL certs with https connexions
+  Add-Type -TypeDefinition @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+      public bool CheckValidationResult(
+      ServicePoint srvPoint, X509Certificate certificate,
+      WebRequest request, int certificateProblem) {
+        return true;
+      }
+    }
+"@
+
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy
+
+}
+
+Function Set-ArubaSWCipherSSL {
+
+  # Hack for allowing TLS 1.1 and TLS 1.2 (by default it is only SSL3 and TLS (1.0))
+  $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+  [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+}
