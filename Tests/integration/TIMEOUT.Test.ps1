18716b3164e876b84234f62040d7115fc4aa701a
Cédric Moreau <moreaucedric0@gmail.com>
2018-10-22 10:16:27 +0200

    Timeout(Test): Add tests for TimeoutConnection (REST) cmdlet
    Part of #36

---
+++ Tests/integration/TIMEOUT.Test.ps1
@@ -0,0 +1,51 @@
+#
+# Copyright 2018, Alexis La Goutte <alexis.lagoutte at gmail dot com>
+# Copyright 2018, Cédric Moreau <moreaucedric0 at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+../common.ps1
+
+Describe  "Get RestSessionTimeout" {
+    It "Get RestSessionTimeout Does not throw an error" {
+        {
+            Get-ArubaSWRestSessionTimeout
+        } | Should Not Throw 
+    }
+
+    It "Get RestSessionTimeout" {
+        $timeout = Get-ArubaSWRestSessionTimeout
+        $timeout | Should not be $NULL
+    }
+
+    It "Get RestSessionTimeout equals 600" {
+        {
+            Get-ArubaSWRestSessionTimeout
+        } | Should be "600"
+    }
+}
+
+Describe  "Set RestSessionTimeout" {
+    It "Change SessionTimeout value" {
+        Set-ArubaSWRestSessionTimeout -timeout 800
+        $timeout = Get-ArubaSWRestSessionTimeout
+        $timeout | Should be "800"
+    }
+
+    It "Change SessionTimeout value to defaults (600)" {
+        Set-ArubaSWRestSessionTimeout -timeout 600
+        $timeout = Get-ArubaSWRestSessionTimeout
+        $timeout | Should be "600"
+    }
+
+    It "Check range of RestSessionTimeout value" {
+        $change = 90
+        {Set-ArubaSWRestSessionTimeout -timeout $change} | Should Throw
+        
+        $change = 8500
+        {Set-ArubaSWRestSessionTimeout -timeout $change} | Should Throw
+    }
+}
+
+Disconnect-ArubaSW -noconfirm
