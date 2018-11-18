38b6f23ef3a6f6a69863d33c4c334124e711f627
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-11-18 17:56:11 +0100

    WebRequest: Move Exception check to dedicated function
    Can be reuse by other function

--- PowerArubaSW/Private/Webrequest.ps1
+++ PowerArubaSW/Private/Webrequest.ps1
@@ -3,7 +3,32 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+function Show-ArubaSWWebRequestException() {
+    Param(
+        [parameter(Mandatory = $true)]
+        $Exception
+    )
 
+    If ($Exception.Exception.Response) {
+        $result = $Exception.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($result)
+        $responseBody = $reader.ReadToEnd()
+
+        $responseJson =  $responseBody | ConvertFrom-Json
+
+        Write-Warning "The Switch API sends an error message:"
+        Write-Warning "Error description (code): $($Exception.Exception.Response.StatusDescription) ($($Exception.Exception.Response.StatusCode.Value__))"
+        if($responseBody) {
+            if($responseJson.message) {
+                Write-Warning "Error details: $($responseJson.message)"
+            } else {
+                Write-Warning "Error details: $($responseBody)"
+            }
+        } elseif($Exception.ErrorDetails.Message) {
+            Write-Warning "Error details: $($Exception.ErrorDetails.Message)"
+        }
+    }
+}
 function Invoke-ArubaSWWebRequest(){
 
     Param(
@@ -45,27 +70,7 @@ function Invoke-ArubaSWWebRequest(){
             }
         }
         catch {
-            If ($_.Exception.Response) {
-
-                $result = $_.Exception.Response.GetResponseStream()
-                $reader = New-Object System.IO.StreamReader($result)
-                $responseBody = $reader.ReadToEnd()
-
-                $responseJson =  $responseBody | ConvertFrom-Json
-                
-                Write-Warning "The Switch API sends an error message:"
-                Write-Warning "Error description (code): $($_.Exception.Response.StatusDescription) ($($_.Exception.Response.StatusCode.Value__))"
-                if($responseBody){
-                    if($responseJson.message) {
-                        Write-Warning "Error details: $($responseJson.message)"
-                    } else {
-                        Write-Warning "Error details: $($responseBody)"
-                    }
-                } elseif($_.ErrorDetails.Message) {
-                    Write-Warning "Error details: $($_.ErrorDetails.Message)"
-                }
-
-            }
+            Show-ArubaSWWebRequestException -Exception $_
             throw "Unable to use switch API"
         }
         $response
