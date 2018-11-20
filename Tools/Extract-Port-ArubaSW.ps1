0843292c08012b602cb1354ebd08523c08a35c20
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-11-20 20:38:09 +0100

    ExtractPortAruba (Tools): fix trailing whitespace

--- Tools/Extract-Port-ArubaSW.ps1
+++ Tools/Extract-Port-ArubaSW.ps1
@@ -29,7 +29,7 @@ This script uses Microsoft Excel, the software will need to be installed on the 
 The script can be executed with the following argument list
 
 Extract-Port-ArubaSW.ps1 -CompanyName <company name> -IPSwitch <switch ip> -LoginSwitch <switch login> -PassSwitch <switch password>
-  
+ 
   .EXAMPLE
 The script can be executed without specifying the type of argument as long as the order of the arguments <company name> <switch ip> <switch login> <switch password> is respected
 
@@ -39,7 +39,7 @@ Extract-Port-ArubaSW.ps1 <company name> <switch ip> <switch login> <switch passw
 
 The script can be executed without arguments, questions will be asked to the user during this one.
 
-Extract-Port-ArubaSW.ps1 
+Extract-Port-ArubaSW.ps1
 
   .NOTES
 
@@ -49,7 +49,7 @@ Extract-Port-ArubaSW.ps1
   Script Name: Extract-Port-ArubaSW
 
   #>
-  
+ 
 # Parameters :
 Param(
   [string]$CompanyName,
@@ -113,7 +113,7 @@ if (Get-Module -ListAvailable -Name PowerArubaSW) {
         Write-Host ""
         $exitquit = Read-Host "Press any key to exit"
         break
-       
+      
         }
 
 # Company name request if not given as an argument
@@ -178,17 +178,17 @@ Write-Host ""
 Write-Host ""
 
 #Verification of loginswitch and passswitch variables
-if ((-not [string]::IsNullOrEmpty($loginswitch)) -and (-not [string]::IsNullOrEmpty($PassSwitch))) { 
+if ((-not [string]::IsNullOrEmpty($loginswitch)) -and (-not [string]::IsNullOrEmpty($PassSwitch))) {
 
         $mysecpassword = ConvertTo-SecureString $PassSwitch -AsPlainText -Force
 
         try {
 
                 Connect-ArubaSW -Server $ipswitch -Username $loginswitch -Password $mysecpassword
-                
+               
                 }
 
-        Catch { 
+        Catch {
 
                 Clear-Host
                 Write-Host ""
@@ -211,17 +211,17 @@ if ((-not [string]::IsNullOrEmpty($loginswitch)) -and (-not [string]::IsNullOrEm
                 $exitquit = Read-Host "Press any key to exit"
                 break
 
-                }        
-       
+                }       
+      
 } else {
 
         try {
 
-                Connect-ArubaSW -Server $ipswitch 
-                
+                Connect-ArubaSW -Server $ipswitch
+               
                 }
 
-        Catch { 
+        Catch {
 
                 Clear-Host
                 Write-Host ""
@@ -245,7 +245,7 @@ if ((-not [string]::IsNullOrEmpty($loginswitch)) -and (-not [string]::IsNullOrEm
                 break
 
                 }
-        
+       
         }
 
 # Creation of the run variable
@@ -262,48 +262,48 @@ $resultarray=@()
 
 # Crossing the information of the run variable against the vlanports variable
 foreach ($port in $run) {
- 
+
     $v = $vlanports | Where-Object {$_.port_id -eq $port.id}
     $tagged=$null
-    $w = $lldpremote | Where-Object {$lldpremote.local_port -eq $port.id} 
+    $w = $lldpremote | Where-Object {$lldpremote.local_port -eq $port.id}
 
     $countvlan = -1
-        
+       
         foreach ($vlanp in $v) {
-            
+           
                 $countvlan ++
-                
+               
                 }
 
         $comacountvlan = 0
-      
+     
         foreach ($vlanp in $v) {
-            
+           
             if ($comacountvlan -eq $countvlan) {
 
                                                 $coma = ''
-                                            
+                                           
                                                 } else {
-                                            
+                                           
                                                 $coma = ', '
-                                            
+                                           
                                                 }
-            
+
             if ($vlanp.port_mode -eq "POM_UNTAGGED") {
-            
+
                     $untagged = $vlanp.vlan_id
-                    
+                   
                     }
 
             if ($vlanp.port_mode -eq "POM_TAGGED_STATIC") {
 
                     $tagged += "$($vlanp.vlan_id)$coma"
-                    
+                   
                     }
 
             $comacountvlan++
-      
-        } 
+
+        }
 
     $item = New-Object PSObject
     $item | Add-Member -type NoteProperty -Name 'name' -Value $($port.name)
@@ -314,7 +314,7 @@ foreach ($port in $run) {
     $item | Add-Member -type NoteProperty -Name 'is_port_up' -Value $($port.is_port_up)
     $item | Add-Member -type NoteProperty -Name 'lldp_port_id' -Value $($w.port_id)
     $item | Add-Member -type NoteProperty -Name 'lldp_port_description' -Value $($w.port_description)
-     
+
     $resultarray += $item
 }
 
@@ -322,11 +322,11 @@ foreach ($port in $run) {
 $infoswitch = Get-ArubaSWSystemStatus
 
 # Launching an instance of MS Excel
-$excel = New-Object -ComObject "Excel.Application"            
+$excel = New-Object -ComObject "Excel.Application"           
 $excel.Visible = $True
 $excel.DisplayAlerts = $False
 
-# Creating an Excel sheet + activating the current sheet   
+# Creating an Excel sheet + activating the current sheet  
 $workbook = $excel.Workbooks.Add()
 $sheet = $workbook.Worksheets.Item(1)
 $sheet.Activate() | Out-Null
@@ -342,25 +342,25 @@ Add-ExcelFormatArray -name 'Name :' -letter1 A -letter2 B -emplacementtxt xlRigh
 
 $row++
 $anum++
-$bnum++ 
+$bnum++
 
 Add-ExcelFormatArray -name 'Serial Number :' -letter1 A -letter2 B -emplacementtxt xlRight -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
 $row++
 $anum++
-$bnum++ 
+$bnum++
 
 Add-ExcelFormatArray -name 'Firmware Version :' -letter1 A -letter2 B -emplacementtxt xlRight -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
 $row++
 $anum++
-$bnum++ 
+$bnum++
 
 Add-ExcelFormatArray -name 'Hardware Revision :' -letter1 A -letter2 B -emplacementtxt xlRight -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
 $row++
 $anum++
-$bnum++ 
+$bnum++
 
 Add-ExcelFormatArray -name 'Product Model :' -letter1 A -letter2 B -emplacementtxt xlRight -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
@@ -373,13 +373,13 @@ $bnum = 1
 
     $row++
     $anum++
-    $bnum++ 
-        
+    $bnum++
+
     Add-ExcelFormatArray -name $infoswitch.serial_number -letter1 C -letter2 G -emplacementtxt xlLeft -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
     $row++
     $anum++
-    $bnum++ 
+    $bnum++
 
     Add-ExcelFormatArray -name $infoswitch.firmware_version -letter1 C -letter2 G -emplacementtxt xlLeft -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
@@ -394,7 +394,7 @@ $bnum = 1
     $bnum++
 
     Add-ExcelFormatArray -name $infoswitch.product_model -letter1 C -letter2 G -emplacementtxt xlLeft -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
-    
+
 $row = 10
 $Column = 1
 $anum = 10
@@ -403,37 +403,37 @@ $bnum = 10
 Add-ExcelFormatArray -name 'Name' -letter1 A -letter2 B -emplacementtxt xlCenter -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
 $Column++
-$Column++  
+$Column++
 
 Add-ExcelFormatArray -name 'Port ID' -letter1 C -letter2 D -emplacementtxt xlCenter -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
-$Column++ 
-$Column++ 
+$Column++
+$Column++
 
 Add-ExcelFormatArray -name 'Port Tagged' -letter1 E -letter2 F -emplacementtxt xlCenter -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
-$Column++ 
-$Column++ 
+$Column++
+$Column++
 
 Add-ExcelFormatArray -name 'Port Untagged' -letter1 G -letter2 H -emplacementtxt xlCenter -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
-$Column++ 
-$Column++ 
+$Column++
+$Column++
 
 Add-ExcelFormatArray -name 'LACP Status' -letter1 I -letter2 J -emplacementtxt xlCenter -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
-$Column++ 
-$Column++ 
+$Column++
+$Column++
 
 Add-ExcelFormatArray -name 'Is Port UP' -letter1 K -letter2 L -emplacementtxt xlCenter -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
-$Column++ 
-$Column++ 
+$Column++
+$Column++
 
 Add-ExcelFormatArray -name 'LLDP Port ID' -letter1 M -letter2 N -emplacementtxt xlCenter -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
-$Column++ 
-$Column++ 
+$Column++
+$Column++
 
 Add-ExcelFormatArray -name 'LLDP Port Description' -letter1 O -letter2 P -emplacementtxt xlCenter -fontcolorindex 55 -fontsize 10 -interiorcolorindex 15
 
@@ -449,38 +449,38 @@ foreach ($entry in $entries)  {
 
     Add-ExcelFormatArray -name $entry.name -letter1 A -letter2 B -emplacementtxt xlCenter -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
-    $Column++ 
+    $Column++
     $Column++
 
     Add-ExcelFormatArray -name $entry.port_id -letter1 C -letter2 D -emplacementtxt xlCenter -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
-    $Column++ 
+    $Column++
     $Column++
 
     Add-ExcelFormatArray -name $entry.port_tagged -letter1 E -letter2 F -emplacementtxt xlCenter -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
-    $Column++ 
+    $Column++
     $Column++
 
     Add-ExcelFormatArray -name $entry.port_untagged -letter1 G -letter2 H -emplacementtxt xlCenter -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
-    $Column++ 
+    $Column++
     $Column++
 
     Add-ExcelFormatArray -name $entry.lacp_status -letter1 I -letter2 J -emplacementtxt xlCenter -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
-    $Column++ 
+    $Column++
     $Column++
 
     Add-ExcelFormatArray -name $entry.is_port_up -letter1 K -letter2 L -emplacementtxt xlCenter -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
-    $Column++ 
-    $Column++    
+    $Column++
+    $Column++   
 
     Add-ExcelFormatArray -name $entry.lldp_port_id -letter1 M -letter2 N -emplacementtxt xlCenter -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
+    $Column++
     $Column++ 
-    $Column++  
 
     Add-ExcelFormatArray -name $entry.lldp_port_description -letter1 O -letter2 P -emplacementtxt xlCenter -fontcolorindex 1 -fontsize 10 -interiorcolorindex 19
 
@@ -488,7 +488,7 @@ foreach ($entry in $entries)  {
     $column=1
     $anum ++
     $bnum ++
-    
+   
     }
 
 # Saving the excel file:
