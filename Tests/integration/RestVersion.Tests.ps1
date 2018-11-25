834fa9c1cac2530a09447568788513333a1a14b4
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-11-25 17:15:13 +0100

    RestVersion(Tests): Add test for RestVersion (Get-ArubaSWRestVersion)
    Part of #36

---
+++ Tests/integration/RestVersion.Tests.ps1
@@ -0,0 +1,23 @@
+#
+# Copyright 2018, Alexis La Goutte <alexis.lagoutte at gmail dot com>
+# Copyright 2018, Cédric Moreau <moreaucedric0 at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+../common.ps1
+
+Describe  "Get-ArubaSWRestVersion" {
+    It "Get-ArubaSWRestVersion Does not throw an error" {
+        {
+            Get-ArubaSWRestVersion
+        } | Should Not Throw 
+    }
+
+    It "Get-ArubaSWRestVersion should not be null" {
+        $version = Get-ArubaSWRestVersion
+        $version.version | Should not be $NULL
+    }
+}
+
+Disconnect-ArubaSW -noconfirm
