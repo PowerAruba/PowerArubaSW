74228e9cf7e80c8c9d60acdd8fb89445f0c2c229
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-02-18 15:18:25 +0100

    Led (Tests): fix Led Test when launch on VSF/Stacked Switches
    Only check info of first switch

--- Tests/integration/Led.Tests.ps1
+++ Tests/integration/Led.Tests.ps1
@@ -19,10 +19,10 @@ Describe  "Get Led Locator" {
 
     It "Get LedLocator info (first unit)" {
         $LED = Get-ArubaSWLed
-        $LED.status | Should Be "LS_OFF"
-        $LED.duration_in_seconds | Should BeNullOrEmpty
-        $LED.when | Should be "LBT_NOW"
-        $LED.remaining_seconds | Should BeNullOrEmpty
+        $LED[0].status | Should Be "LS_OFF"
+        $LED[0].duration_in_seconds | Should BeNullOrEmpty
+        $LED[0].when | Should be "LBT_NOW"
+        $LED[0].remaining_seconds | Should BeNullOrEmpty
     }
 }
 
@@ -31,75 +31,75 @@ Describe  "Configure Led Locator" {
     It "Configure Led Locator (with default duration => 30 minutes)" {
         Set-ArubaSWLed -status on
         $LED = Get-ArubaSWLed
-        $LED.status | Should Be "LS_ON"
-        $LED.duration_in_seconds | Should BeNullOrEmpty
-        $LED.when | Should be "LBT_NOW"
-        $LED.remaining_seconds | Should -BeGreaterThan 1740
+        $LED[0].status | Should Be "LS_ON"
+        $LED[0].duration_in_seconds | Should BeNullOrEmpty
+        $LED[0].when | Should be "LBT_NOW"
+        $LED[0].remaining_seconds | Should -BeGreaterThan 1740
     }
 
     It "Configure Led Locator (with duration 1 minute)" {
         Set-ArubaSWLed -status on -duration 1
         Start-Sleep 1
         $LED = Get-ArubaSWLed
-        $LED.status | Should Be "LS_ON"
-        $LED.duration_in_seconds | Should BeNullOrEmpty
-        $LED.when | Should be "LBT_NOW"
-        $LED.remaining_seconds | Should -BeGreaterThan 40
-        $LED.remaining_seconds | Should -BeLessThan 60
+        $LED[0].status | Should Be "LS_ON"
+        $LED[0].duration_in_seconds | Should BeNullOrEmpty
+        $LED[0].when | Should be "LBT_NOW"
+        $LED[0].remaining_seconds | Should -BeGreaterThan 40
+        $LED[0].remaining_seconds | Should -BeLessThan 60
     }
 
     It "Disable Led Locator" {
         Set-ArubaSWLed -status off
         $LED = Get-ArubaSWLed
-        $LED.status | Should Be "LS_OFF"
-        $LED.duration_in_seconds | Should BeNullOrEmpty
-        $LED.when | Should be "LBT_NOW"
-        $LED.remaining_seconds | Should BeNullOrEmpty
+        $LED[0].status | Should Be "LS_OFF"
+        $LED[0].duration_in_seconds | Should BeNullOrEmpty
+        $LED[0].when | Should be "LBT_NOW"
+        $LED[0].remaining_seconds | Should BeNullOrEmpty
     }
 
     It "Configure Led locator for blink" {
         Set-ArubaSWLed -status blink
         $LED = Get-ArubaSWLed
-        $LED.status | Should Be "LS_BLINK"
-        $LED.duration_in_seconds | Should BeNullOrEmpty
-        $LED.when | Should be "LBT_NOW"
-        $LED.remaining_seconds | Should -BeGreaterThan 1740
+        $LED[0].status | Should Be "LS_BLINK"
+        $LED[0].duration_in_seconds | Should BeNullOrEmpty
+        $LED[0].when | Should be "LBT_NOW"
+        $LED[0].remaining_seconds | Should -BeGreaterThan 1740
     }
 
     It "Disable Led locator blinking" {
         Set-ArubaSWLed -status off
         $LED = Get-ArubaSWLed
-        $LED.status | Should Be "LS_OFF"
-        $LED.duration_in_seconds | Should BeNullOrEmpty
-        $LED.when | Should be "LBT_NOW"
-        $LED.remaining_seconds | Should BeNullOrEmpty
+        $LED[0].status | Should Be "LS_OFF"
+        $LED[0].duration_in_seconds | Should BeNullOrEmpty
+        $LED[0].when | Should be "LBT_NOW"
+        $LED[0].remaining_seconds | Should BeNullOrEmpty
     }
 
     It "Configure Led locator to startup" {
         Set-ArubaSWLed -status on -when Startup
         $LED = Get-ArubaSWLed
-        $LED.status | Should Be "LS_ON"
-        $LED.duration_in_seconds | Should be 1800
-        $LED.when | Should be "LBT_STARTUP"
-        $LED.remaining_seconds | Should BeNullOrEmpty
+        $LED[0].status | Should Be "LS_ON"
+        $LED[0].duration_in_seconds | Should be 1800
+        $LED[0].when | Should be "LBT_STARTUP"
+        $LED[0].remaining_seconds | Should BeNullOrEmpty
     }
 
     It "Configure Led locator to startup during 1minute" {
         Set-ArubaSWLed -status on -when Startup -duration 1
         $LED = Get-ArubaSWLed
-        $LED.status | Should Be "LS_ON"
-        $LED.duration_in_seconds | Should be 60
-        $LED.when | Should be "LBT_STARTUP"
-        $LED.remaining_seconds | Should BeNullOrEmpty
+        $LED[0].status | Should Be "LS_ON"
+        $LED[0].duration_in_seconds | Should be 60
+        $LED[0].when | Should be "LBT_STARTUP"
+        $LED[0].remaining_seconds | Should BeNullOrEmpty
     }
 
     It "Disable Led locator blinking" {
         Set-ArubaSWLed -status off
         $LED = Get-ArubaSWLed
-        $LED.status | Should Be "LS_OFF"
-        $LED.duration_in_seconds | Should BeNullOrEmpty
-        $LED.when | Should be "LBT_NOW"
-        $LED.remaining_seconds | Should BeNullOrEmpty
+        $LED[0].status | Should Be "LS_OFF"
+        $LED[0].duration_in_seconds | Should BeNullOrEmpty
+        $LED[0].when | Should be "LBT_NOW"
+        $LED[0].remaining_seconds | Should BeNullOrEmpty
     }
 }
 
