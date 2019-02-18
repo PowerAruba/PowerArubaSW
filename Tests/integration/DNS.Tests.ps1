d7dc83eaf60b7fa65f72d792e4beb41a233b88a2
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-02-18 16:27:10 +0100

    DNS(tests): use 2 different DNS provider :-)
    Cloudflare DNS (1.1.1.1) and Google DNS (8.8.8.8)

--- Tests/integration/DNS.Tests.ps1
+++ Tests/integration/DNS.Tests.ps1
@@ -15,23 +15,23 @@ Describe  "Get-ArubaSWDns" {
 
 Describe  "Set-ArubaSWDns" {
     It "Set ArubaSWDns ip server 1" {
-        Set-ArubaSWDns -mode Manual -server1 8.8.8.8
+        Set-ArubaSWDns -mode Manual -server1 1.1.1.1
         $dns = Get-ArubaSWDns
-        $dns.server_1.octets | Should be "8.8.8.8"
+        $dns.server_1.octets | Should be "1.1.1.1"
         Remove-ArubaSWDns -mode Manual
     }
 
     It "Set ArubaSWDns ip server 2" {
-        Set-ArubaSWDns -mode Manual -server2 8.8.4.4
+        Set-ArubaSWDns -mode Manual -server2 8.8.8.8
         $dns = Get-ArubaSWDns
-        $dns.server_2.octets | Should be "8.8.4.4"
+        $dns.server_2.octets | Should be "8.8.8.8"
         Remove-ArubaSWDns -mode Manual
     }
 
     It "Set ArubaSWDns dns domain names" {
-        Set-ArubaSWDns -mode Manual -server1 8.8.4.4 -server2 8.8.8.8 -domain example.org
+        Set-ArubaSWDns -mode Manual -server1 1.1.1.1 -server2 8.8.8.8 -domain example.org
         $dns = Get-ArubaSWDns
-        $dns.server_1.octets | Should be "8.8.4.4"
+        $dns.server_1.octets | Should be "1.1.1.1"
         $dns.server_2.octets | Should be "8.8.8.8"
         $dns.dns_domain_names | Should be "example.org"
         Remove-ArubaSWDns -mode Manual
