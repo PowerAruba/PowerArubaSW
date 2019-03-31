edbd7de7a1d87614970482b1c2a3f3e5055f0eb5
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-03-31 16:31:13 +0200

    IPAddress(tests): Add more test for Get-ArubaSWIPAddress

--- Tests/integration/IpAddress.Tests.ps1
+++ Tests/integration/IpAddress.Tests.ps1
@@ -9,6 +9,13 @@
 
 Describe  "Get-ArubaSWIPAddress" {
     It "Get-ArubaSWIPAddress Does not throw an error" {
-        { Get-ArubaSWIPAddress } | Should Not Throw 
+        { Get-ArubaSWIPAddress } | Should Not Throw
+    }
+
+    It "Get IP Address(es) of Switch" {
+        $ipaddress = Get-ArubaSWIPAddress
+        $ipaddress | Should not be $NULL
+        $ipaddress.ip_address_mode | Should -BeOfType string
+        $ipaddress.vlan_id | Should -BeOfType int
     }
 }
