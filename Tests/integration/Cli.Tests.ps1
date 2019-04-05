c00d6a24a86c558bf0ce2386517509efd357de48
Cédric Moreau <moreaucedric0@gmail.com>
2019-04-05 17:38:31 +0200

    Optimize the tests

--- Tests/integration/Cli.Tests.ps1
+++ Tests/integration/Cli.Tests.ps1
@@ -7,10 +7,6 @@
 
 . ../common.ps1
 
-$mysecpassword = ConvertTo-SecureString $password -AsPlainText -Force
-Connect-ArubaSW -Server $ipaddress -Username $login -password $mysecpassword -SkipCertificateCheck
-
-
 Describe  "Get-ArubaSWCli" {
     It "Get-ArubaSWCli Does not throw an error" {
         { Get-ArubaSWCli -cmd "show run" } | Should Not Throw 
@@ -18,17 +14,11 @@ Describe  "Get-ArubaSWCli" {
     It "Get-ArubaSWCli Should not be null" {
         { Get-ArubaSWCli -cmd "show run" } | Should not be $NULL
     }
-    It "Value of status should be a success" {
+    It "Value of status should be a success, sohould not be null, and should have the right command" {
         $cli = Get-ArubaSWCli -cmd "show run"
         $cli.status | Should be "CCS_SUCCESS"
-    }
-    It "Value of cmd should be the command given as parameter" {
-        $cli = Get-ArubaSWCli -cmd "show run"
-        $cli.cmd | Should be "show run"
-    }
-    It "Value of result_base64_encoded should not be null" {
-        $cli = Get-ArubaSWCli -cmd "show run"
         $cli.result_base64_encoded | Should not be $NULL
+        $cli.cmd | Should be "show run"
     }
 }
 
