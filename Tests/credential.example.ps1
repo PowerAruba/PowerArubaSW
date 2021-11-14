a940a96d878e092e241f4a30e6ff8ba352ae19e7
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-11-14 16:24:31 +0100

    credential.example.ps1: Fix PSUseDeclaredVarsMoreThanAssignments by using script:

--- Tests/credential.example.ps1
+++ Tests/credential.example.ps1
@@ -6,25 +6,25 @@
 
 # Copy this file to credential.ps1 (on Tests folder) and change connection settings..
 
-$ipaddress = "10.44.23.213"
-$login = "admin"
-$password = "enable"
-$httpOnly = $false
+$script:ipaddress = "10.44.23.213"
+$script:login = "admin"
+$script:password = "enable"
+$script:httpOnly = $false
 
 
 #default settings use for test, can be override if needed...
 
-#$pester_vlan = 85
-#$pester_vlanport = 8
-#$pester_lacp_port = 5
-#$pester_lacp_trk1 = "trk2"
-#$pester_lacp_trk2 = "trk6"
-#$pester_port = 3
-#$pester_stack_module = 1
-#$pester_chassis_module = "A"
-#$pester_trunk_port = 5
-#$pester_trunk_trk1 = "trk3"
-#$pester_trunk_trk2 = "trk7"
-#$pester_stp_port = 3
-#$pester_cli_port = 3
-#$pester_poe_port = 4
+#$script:pester_vlan = 85
+#$script:pester_vlanport = 8
+#$script:pester_lacp_port = 5
+#$script:pester_lacp_trk1 = "trk2"
+#$script:pester_lacp_trk2 = "trk6"
+#$script:pester_port = 3
+#$script:pester_stack_module = 1
+#$script:pester_chassis_module = "A"
+#$script:pester_trunk_port = 5
+#$script:pester_trunk_trk1 = "trk3"
+#$script:pester_trunk_trk2 = "trk7"
+#$script:pester_stp_port = 3
+#$script:pester_cli_port = 3
+#$script:pester_poe_port = 4
