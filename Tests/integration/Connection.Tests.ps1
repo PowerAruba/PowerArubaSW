e82cd4e79f5c7282412d873de3b4016acaf23d39
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-11-25 20:48:02 +0100

    Connection(Multi): Add Also missing RADIUS Server...)

--- Tests/integration/Connection.Tests.ps1
+++ Tests/integration/Connection.Tests.ps1
@@ -88,6 +88,9 @@ Describe  "Connect to a switch (using multi connection)" {
         It "Use Multi connection for call Get RADIUS Profile" {
             { Get-ArubaSWRadiusProfile -connection $sw } | Should Not throw
         }
+        It "Use Multi connection for call Get RADIUS Server" {
+            { Get-ArubaSWRadiusServer -connection $sw } | Should Not throw
+        }
     }
 
     It "Disconnect to a switch (Multi connection)" {
