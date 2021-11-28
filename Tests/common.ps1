6e16b2efb4654c58fe4408fcefc2016916d623e6
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-11-28 09:32:59 +0100

    common(Tests): fix scope for variable (use script:)
    Fix also CliBatch Tests... :)

--- Tests/common.ps1
+++ Tests/common.ps1
@@ -9,22 +9,22 @@
 Param()
 
 # default settings for test...
-$pester_vlan = 85 #vlan id for Vlan test (and Port Test)
-$pester_vlanport = 8 #Port number of Vlan Port Test
-$pester_lacp_port = 5 #Port number of LACP test
-$pester_lacp_trk1 = "trk2" #Port trunk 1 name of LACP test
-$pester_lacp_trk2 = "trk6" #Port trunk 2 name of LACP test
-$pester_port = 3 #Port number of port test
-$pester_stack_module = 1 #Number of stack moduele (for VSF/Stack)
-$pester_chassis_module = "A" #Letter of chassis module (for HP54XXRzl2)
-$pester_trunk_port = 5 #Port number of LACP test
-$pester_trunk_trk1 = "trk3" #Port trunk 1 name of Trunk test
-$pester_trunk_trk2 = "trk7" #Port trunk 2 name of Trunk test
-$pester_stp_port = 3 #Port Number of STP test
-$pester_cli_port = 3 #Port Number of CLI test
-$pester_poe_port = 4 #Port Number of PoE test
-$pester_dns1 = "1.1.1.1" #DNS server 1
-$pester_dns2 = "8.8.8.8" #DNS server 2
+$script:pester_vlan = 85 #vlan id for Vlan test (and Port Test)
+$script:pester_vlanport = 8 #Port number of Vlan Port Test
+$script:pester_lacp_port = 5 #Port number of LACP test
+$script:pester_lacp_trk1 = "trk2" #Port trunk 1 name of LACP test
+$script:pester_lacp_trk2 = "trk6" #Port trunk 2 name of LACP test
+$script:pester_port = 3 #Port number of port test
+$script:pester_stack_module = 1 #Number of stack moduele (for VSF/Stack)
+$script:pester_chassis_module = "A" #Letter of chassis module (for HP54XXRzl2)
+$script:pester_trunk_port = 5 #Port number of LACP test
+$script:pester_trunk_trk1 = "trk3" #Port trunk 1 name of Trunk test
+$script:pester_trunk_trk2 = "trk7" #Port trunk 2 name of Trunk test
+$script:pester_stp_port = 3 #Port Number of STP test
+$script:pester_cli_port = 3 #Port Number of CLI test
+$script:pester_poe_port = 4 #Port Number of PoE test
+$script:pester_dns1 = "1.1.1.1" #DNS server 1
+$script:pester_dns2 = "8.8.8.8" #DNS server 2
 
 if ("Desktop" -eq $PSVersionTable.PsEdition) {
     # -BeOfType is not same on PowerShell Core and Desktop (get int with Desktop and long with Core for number)
@@ -81,24 +81,24 @@ else {
 }
 #Add chassis module (letter) to port number if it is a HP 5406Rzl2 (J9850A) or HP 5412Rzl2 (J9851A)
 if ($product_number -eq 'J9850A' -or $product_number -eq 'J9851A') {
-    $pester_vlanport = "$pester_chassis_module$pester_vlanport"
-    $pester_lacp_port = "$pester_chassis_module$pester_lacp_port"
-    $pester_port = "$pester_chassis_module$pester_$pester_port"
-    $pester_trunk_port = "$pester_chassis_module$pester_trunk_port"
-    $pester_stp_port = "$pester_chassis_module$pester_stp_port"
-    $pester_cli_port = "$pester_chassis_module$pester_cli_port"
-    $pester_poe_port = "$pester_chassis_module$pester_poe_port"
+    $script:pester_vlanport = "$pester_chassis_module$pester_vlanport"
+    $script:pester_lacp_port = "$pester_chassis_module$pester_lacp_port"
+    $script:pester_port = "$pester_chassis_module$pester_$pester_port"
+    $script:pester_trunk_port = "$pester_chassis_module$pester_trunk_port"
+    $script:pester_stp_port = "$pester_chassis_module$pester_stp_port"
+    $script:pester_cli_port = "$pester_chassis_module$pester_cli_port"
+    $script:pester_poe_port = "$pester_chassis_module$pester_poe_port"
 }
 
 #Add stack module to port number (if it is a stacked switch)
 if ('ST_STACKED' -eq $defaultArubaSWConnection.switch_type) {
-    $pester_vlanport = "$pester_stack_module/$pester_vlanport"
-    $pester_lacp_port = "$pester_stack_module/$pester_lacp_port"
-    $pester_port = "$pester_stack_module/$pester_port"
-    $pester_trunk_port = "$pester_stack_module/$pester_trunk_port"
-    $pester_stp_port = "$pester_stack_module/$pester_stp_port"
-    $pester_cli_port = "$pester_stack_module/$pester_cli_port"
-    $pester_poe_port = "$pester_stack_module/$pester_poe_port"
+    $script:pester_vlanport = "$pester_stack_module/$pester_vlanport"
+    $script:pester_lacp_port = "$pester_stack_module/$pester_lacp_port"
+    $script:pester_port = "$pester_stack_module/$pester_port"
+    $script:pester_trunk_port = "$pester_stack_module/$pester_trunk_port"
+    $script:pester_stp_port = "$pester_stack_module/$pester_stp_port"
+    $script:pester_cli_port = "$pester_stack_module/$pester_cli_port"
+    $script:pester_poe_port = "$pester_stack_module/$pester_poe_port"
 }
 
 Disconnect-ArubaSW -noconfirm
