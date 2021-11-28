bc17fa934c56e6fd40275942ee92a13c8f9b839f
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-11-28 20:23:37 +0100

    MacTable(tests): Convert to Pester v5

--- Tests/integration/MacTable.Tests.ps1
+++ Tests/integration/MacTable.Tests.ps1
@@ -5,6 +5,10 @@
 #
 . ../common.ps1
 
+BeforeAll {
+    Connect-ArubaSW @invokeParams
+}
+
 Describe  "Get Mac Table" {
 
     BeforeAll {
@@ -18,22 +22,22 @@ Describe  "Get Mac Table" {
     It "Get Mac Table Does not throw an error" {
         {
             Get-ArubaSWMacTable
-        } | Should Not Throw
+        } | Should -Not -Throw
     }
 
     It "Get ALL Mac Table" {
         $mactable = Get-ArubaSWMacTable
-        $mactable.count | Should not be $NULL
+        $mactable.count | Should -Not -Be $NULL
     }
 
     It "Get the Mac Table by Port id ($pester_mactable_port_id)" {
         $mactable = Get-ArubaSWMacTable -port_id $pester_mactable_port_id
-        $mactable[0].port_id | Should be $pester_mactable_port_id
+        $mactable[0].port_id | Should -Be $pester_mactable_port_id
     }
 
     It "Get the Mac Table by Vlan id ($pester_mactable_vlan_id)" {
         $mactable = Get-ArubaSWMacTable -vlan_id $pester_mactable_vlan_id
-        $mactable[0].vlan_id | Should be $pester_mactable_vlan_id
+        $mactable[0].vlan_id | Should -Be $pester_mactable_vlan_id
     }
 
     Context "Get the Mac Table by MAC Address ($pester_mactable_mac_address)" {
@@ -54,23 +58,25 @@ Describe  "Get Mac Table" {
 
         It "Get the Mac Table by MAC Address with colon ($mac_colon)" {
             $mactable = Get-ArubaSWMacTable -mac_address $mac_colon
-            $mactable.mac_address | Should be $pester_mactable_mac_address
+            $mactable.mac_address | Should -Be $pester_mactable_mac_address
         }
         It "Get the Mac Table by MAC Address with dash ($mac_dash)" {
             $mactable = Get-ArubaSWMacTable -mac_address $mac_dash
-            $mactable.mac_address | Should be $pester_mactable_mac_address
+            $mactable.mac_address | Should -Be $pester_mactable_mac_address
         }
 
         It "Get the Mac Table by MAC Address with central dash ($pester_mactable_mac_address)" {
             $mactable = Get-ArubaSWMacTable -mac_address $pester_mactable_mac_address
-            $mactable.mac_address | Should be $pester_mactable_mac_address
+            $mactable.mac_address | Should -Be $pester_mactable_mac_address
         }
 
         It "Get the Mac Table by MAC Address with no space ($mac_nospace))" {
             $mactable = Get-ArubaSWMacTable -mac_address $mac_nospace
-            $mactable.mac_address | Should be $pester_mactable_mac_address
+            $mactable.mac_address | Should -Be $pester_mactable_mac_address
         }
     }
 }
 
-Disconnect-ArubaSW -noconfirm
+AfterAll {
+    Disconnect-ArubaSW -noconfirm
+}
