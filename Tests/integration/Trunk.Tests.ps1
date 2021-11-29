5282ef5104989d6719fbda60f123f531c7f6934a
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-11-29 08:12:11 +0100

    Trunk(tests): Convert to Pester v5

--- Tests/integration/Trunk.Tests.ps1
+++ Tests/integration/Trunk.Tests.ps1
@@ -7,9 +7,13 @@
 
 . ../common.ps1
 
+BeforeAll {
+    Connect-ArubaSW @invokeParams
+}
+
 Describe  "Get Aruba Trunk" {
     It "Get ArubaSWTrunk Does not throw an error" {
-        { Get-ArubaSWTrunk } | Should Not Throw
+        { Get-ArubaSWTrunk } | Should -Not -Throw
     }
 }
 
@@ -17,35 +21,36 @@ Describe  "Add Aruba Trunk" {
     It "Add Trunk $pester_trunk_trk1 on port $pester_trunk_port" {
         Add-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port
         $Trunk = Get-ArubaSWTrunk | Where-Object port_id -eq $pester_trunk_port
-        $Trunk.port_id | Should be "$pester_trunk_port"
-        $Trunk.trunk_group | Should be "$pester_trunk_trk1"
+        $Trunk.port_id | Should -Be "$pester_trunk_port"
+        $Trunk.trunk_group | Should -Be "$pester_trunk_trk1"
         Remove-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port -noconfirm
     }
 
     It "Change trunk group $pester_trunk_trk1 on a port without removing it before" {
         Add-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port
-        { Add-ArubaSWTrunk -trunk_group $pester_trunk_trk2 -port $pester_trunk_port 3> $null } | Should Throw
+        { Add-ArubaSWTrunk -trunk_group $pester_trunk_trk2 -port $pester_trunk_port 3> $null } | Should -Throw
         Remove-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port -noconfirm
     }
 
     It "Change trunk group $pester_trunk_trk2 on a port after removing this port of the trunk group" {
         Add-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port
         Remove-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port -noconfirm
-        { Add-ArubaSWTrunk -trunk_group $pester_trunk_trk2 -port $pester_trunk_port } | Should Not Throw
+        { Add-ArubaSWTrunk -trunk_group $pester_trunk_trk2 -port $pester_trunk_port } | Should -Not -Throw
         Remove-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port -noconfirm
     }
 }
 
 Describe  "Remove Aruba Trunk" {
     It "Remove ArubaSWTrunk does throw an error if trunk group doesn't exist on a port" {
-        { Remove-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port -noconfirm 3> $null } | Should Throw
+        { Remove-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port -noconfirm 3> $null } | Should -Throw
     }
 
     It "Remove ArubaSWTrunk does not throw an error if the trunk group exist on a port" {
         Add-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port
-        { Remove-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port -noconfirm } | Should Not Throw
+        { Remove-ArubaSWTrunk -trunk_group $pester_trunk_trk1 -port $pester_trunk_port -noconfirm } | Should -Not -Throw
     }
 }
 
-
-Disconnect-ArubaSW -noconfirm
+AfterAll {
+    Disconnect-ArubaSW -noconfirm
+}
